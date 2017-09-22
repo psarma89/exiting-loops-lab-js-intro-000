9fde6ddbb72a56c509bd5118fb752bb5e707e31f
Priyam Sarma <priyam.sarma@gmail.com>
2017-09-22 19:07:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,14 @@ function breakOut(array, changeValue, stopValue){
   }
   return array
 }
+
+function keepGoing(array, changeValue, stopValue){
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === stopValue){
+      continue
+    }else {
+      array[i] = changeValue
+    }
+  }
+  return array
+}
